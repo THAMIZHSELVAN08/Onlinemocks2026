2800cb6f7aad8f82e392ad4259897174141e9270
mubashir-73 <mubashirsheriff7@gmail.com>
2026-03-06 17:18:08 +0000

    made minor changes
    # Conflicts:
    #	backend/src/server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,7 +20,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
+<<<<<<< HEAD
     origin: true,
+=======
+    origin:[process.env.FRONTEND_URL,"http://localhost:5173"],
+>>>>>>> 728b6fd (made minor changes)
     methods: ["GET", "POST"],
     credentials: true
   },
@@ -32,12 +36,19 @@ app.use(
     crossOriginResourcePolicy: false, // Required to serve files (resumes)
   }),
 );
+<<<<<<< HEAD
 app.use(
           cors({
                 origin: allowedOrigins,
                 credentials: true,
           })
 );
+=======
+app.use(cors({origin:[process.env.FRONTEND_URL,"http://localhost:5173"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+>>>>>>> 728b6fd (made minor changes)
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.set("socketio", io);
